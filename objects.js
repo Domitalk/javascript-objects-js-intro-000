e5bb85580314ba3665784198c7dc87ce53e2f54a
Dominic <dominic.m.chu@gmail.com>
2019-08-03 02:28:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  var playlist 
-  delete [playlist].artistName;
+  delete playlist.[artistName];
   playlist;
 }
